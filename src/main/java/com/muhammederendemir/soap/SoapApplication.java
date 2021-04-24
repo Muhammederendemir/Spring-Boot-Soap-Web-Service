798d18336800f903c5d59db49baf313183ee7f4c
Muhammederendemir <45883661+mhmmderen3@users.noreply.github.com>
2021-04-24 22:36:15 +0300

    add base

---
+++ src/main/java/com/muhammederendemir/soap/SoapApplication.java
@@ -0,0 +1,13 @@
+package com.muhammederendemir.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muhammederendemir/soap/SoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammederendemir.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
